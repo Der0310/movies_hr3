ef2cdfe9522ca0fd221dae2ff16a2388ac3d2168
Hider Rojas <hiderrojas@gmail.com>
2024-06-10 12:55:56 -0600

    fifth commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 
 const app = require('./app');
 const sequelize = require('./utils/connection');
-require('./models');
+require('./models/Index');
 const PORT = process.env.PORT || 8080;
 
 const main = async () => {
